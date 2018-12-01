9a95c08cee1ce3b8a354b8604a40b085a1473a89
Dean Gramcko <dgramcko@csus.edu>
2018-12-01 14:55:12 -0800

    Finished general layout for loading tab

--- PIrexApplicationGUI/src/app/PirexLoadingTab.java
+++ PIrexApplicationGUI/src/app/PirexLoadingTab.java
@@ -4,11 +4,15 @@ import javax.swing.JSeparator;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
@@ -18,9 +22,11 @@ import javax.swing.JTextField;
 public class PirexLoadingTab extends JPanel 
 {
 	
-	private JTextField jtfFilePath = new JTextField(), jtfTitle = new JTextField(), jtfAuthor = new JTextField();
-	private JButton jbBrowse = new JButton("Browse");
-	private JComboBox jcbFileType = new JComboBox();
+	private JTextField jtfFilePath = new JTextField(80), jtfTitle = new JTextField(45), jtfAuthor = new JTextField(40);
+	private JButton jbBrowse = new JButton("Browse"), jbProcess = new JButton("Process");
+	private String[] comboBoxOptions = new String[] {"Project Gutenberg File", "Plain Text File"};
+	private JComboBox<String> jcbFileType = new JComboBox<String>(comboBoxOptions);
+	private JPanel jpLoad = new JPanel(); // this panel is the load summary window
 	
 	PirexLoadingTab()
 	{
@@ -29,22 +35,40 @@ public class PirexLoadingTab extends JPanel
 		loadTab.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		
-		JPanel row1 = new JPanel(), row2 = new JPanel(), row3 = new JPanel(), jpLoadSummary = new JPanel();
+		JPanel row1 = new JPanel(), row2 = new JPanel(), row3 = new JPanel(),row4 = new JPanel();
 		gbc.insets = new Insets(5,5,5,5);
 		
-		FlowLayout rowLayout = new FlowLayout(); // controls layout for rows 1 through 3
+		FlowLayout rowLayout = new FlowLayout(-0,7, 0); // controls layout for rows 1 through 3
 		row1.setLayout(rowLayout);
 		row2.setLayout(rowLayout);
 		row3.setLayout(rowLayout);
+		row4.setLayout(rowLayout);
 
 		//build row 1
+		row1.add(new JLabel("Text File"));
+		row1.add(jtfFilePath);
+		row1.add(jbBrowse);
+		
+		//build row 2
+		row2.add(new JLabel("Text File Type"));
+		jcbFileType.setPrototypeDisplayValue((String)"QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"); //set default width
+		row2.add(jcbFileType);
+		
+		//build row 3
+		row3.add(new JLabel("Title"));
+		row3.add(jtfTitle);
+		row3.add(new JLabel("Author"));
+		row3.add(jtfAuthor);
+		
+		//build row4
+		row4.add(jbProcess);
 		
 		
+		//set up Load Summary Display Window
 		
-		
-		
-		
-		
+		jpLoad.setBorder(BorderFactory.createEtchedBorder());
+		jpLoad.setBackground(Color.WHITE);
+		//jpLoad.setMinimumSize(new Dimension(jpLoad.getPreferredSize().width, 500));
 		
 		//Add subcomponents to top-level container
 		gbc.weightx = 1;
@@ -64,14 +88,20 @@ public class PirexLoadingTab extends JPanel
 		
 		
 		JSeparator hrule = new JSeparator();
+		hrule.setBackground(Color.GRAY);
 		gbc.gridx = 0;
 		gbc.gridy = 3;
 		loadTab.add(hrule, gbc);
 		
-
 		gbc.gridx = 0;
-		gbc.gridy = 3;
-		loadTab.add(jpLoadSummary,gbc);
+		gbc.gridy = 4;
+		loadTab.add(row4, gbc);
+		
+		gbc.weighty=1;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		loadTab.add(jpLoad,gbc);
 		
 		
 		
