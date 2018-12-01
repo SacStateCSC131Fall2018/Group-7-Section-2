15ac9279c42ee28d474992f50ef84f0c1832b217
shazimajaz1 <shazimajaz@192.168.43.209>
2018-12-01 12:32:02 -0800

    Removed Package Declarations

---
+++ GUI/app/PirexFrame.java
@@ -0,0 +1,35 @@
+
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+/*
+ * This class draws the main PirexWindow on the Screen
+ * A new object of this class must be created to use
+ * at. All smaller containers can be inserted into this 
+ * main frame.
+ */
+public class PirexFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8008539775178469403L;
+
+	/*
+	 * Default Constructor: constructs a new PirexFrame with Name "Pirex"
+	 * The default layout used in BorderLayout
+	 * 
+	 */
+	public PirexFrame() {
+		
+		super("Pirex");
+		setLayout(new BorderLayout());
+		//setSize(780, 520);
+		//setResizable(false);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+}
---
+++ GUI/app/PirexSearchPanel.java
@@ -0,0 +1,45 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PirexSearchPanel extends JPanel
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2664023311134068156L;
+
+	public PirexSearchPanel()
+	{
+		super(new FlowLayout());
+		addQuery();
+		addClearButton();
+		addResultsWindow();
+		addPreviewWindow();
+	}
+
+	private void addQuery()
+	{
+		// TODO Auto-generated method stub
+
+		
+	}
+
+	private void addClearButton()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addResultsWindow()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addPreviewWindow()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
