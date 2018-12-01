0272bfa00c7780260a7b638a82c659ce6772464e
Dean Gramcko <dgramcko@csus.edu>
2018-12-01 14:04:48 -0800

    Redesigned class to separate rows into their own JPanels and use flow layout on them. Still building rows.

--- PIrexApplicationGUI/src/app/PirexLoadingTab.java
+++ PIrexApplicationGUI/src/app/PirexLoadingTab.java
@@ -1,8 +1,11 @@
 
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -10,17 +13,11 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 
-/*	11-30-18
- *  This is obviously incomplete. I still need to work out some issues with spacing components on each row. I'll probably end up moving each row into it's own JPanel and just
- *  deal with it that way, will try tomorrow.
- * 
- */
-
 
 @SuppressWarnings("serial") // this class does not employ serialization.
 public class PirexLoadingTab extends JPanel 
 {
-	private GridBagConstraints gbc = new GridBagConstraints();
+	
 	private JTextField jtfFilePath = new JTextField(), jtfTitle = new JTextField(), jtfAuthor = new JTextField();
 	private JButton jbBrowse = new JButton("Browse");
 	private JComboBox jcbFileType = new JComboBox();
@@ -28,67 +25,56 @@ public class PirexLoadingTab extends JPanel
 	PirexLoadingTab()
 	{
 		super();
-		this.setLayout(new GridBagLayout());
+		Container loadTab = this;
+		loadTab.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		JPanel row1 = new JPanel(), row2 = new JPanel(), row3 = new JPanel(), jpLoadSummary = new JPanel();
 		gbc.insets = new Insets(5,5,5,5);
 		
-		//row 1 components
-		gbc.anchor = GridBagConstraints.WEST;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		add(new JLabel("Text File"), gbc);
+		FlowLayout rowLayout = new FlowLayout(); // controls layout for rows 1 through 3
+		row1.setLayout(rowLayout);
+		row2.setLayout(rowLayout);
+		row3.setLayout(rowLayout);
+
+		//build row 1
 		
+		
+		
+		
+		
+		
+		
+		
+		//Add subcomponents to top-level container
+		gbc.weightx = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		
-		gbc.weightx=1;
-		
-		gbc.gridx = 1;
+		gbc.gridx = 0;
 		gbc.gridy = 0;
-		add(jtfFilePath, gbc);
+		loadTab.add(row1, gbc);
 		
-		gbc.weightx=0;
-		
-		gbc.gridx=2;
-		gbc.gridy=0;
-		add(jbBrowse, gbc);
-		
-		
-		// row 2 components
 		gbc.gridx = 0;
 		gbc.gridy = 1;
-		add(new JLabel("Text File Type"), gbc);
-
-		gbc.gridwidth = 2;
-
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		add(jcbFileType, gbc);
-
-		//row 3 components
-		gbc.gridwidth = 1;
-		gbc.weightx=0;
+		loadTab.add(row2, gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 2;
-		add(new JLabel("Title"), gbc);
+		loadTab.add(row3, gbc);
+		
+		
+		JSeparator hrule = new JSeparator();
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		loadTab.add(hrule, gbc);
+		
 
-		gbc.weightx=1;
-		
-		gbc.gridx = 1;
-		gbc.gridy = 2;
-		add(jtfTitle, gbc);
-		
-		gbc.weightx=0;
-
-		gbc.gridx = 2;
-		gbc.gridy = 2;
-		add(new JLabel("Author"), gbc);
-
-		gbc.weightx=1;
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		loadTab.add(jpLoadSummary,gbc);
 		
 		
-		gbc.gridx = 3;
-		gbc.gridy = 2;
-		add(jtfAuthor, gbc);
+		
 	}
 	
 }
