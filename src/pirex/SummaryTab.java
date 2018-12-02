18f77902ef8f1abb1d238fda3311fe1085568fad
SHendryx <samuelhendryx@csus.edu>
2018-12-02 12:44:32 -0800

    Made Summary Screen resize gracefully.

--- src/pirex/SummaryTab.java
+++ src/pirex/SummaryTab.java
@@ -4,6 +4,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import java.awt.*;
 
 /*
  * This class creates a summary tab.
@@ -11,22 +12,35 @@ import javax.swing.JTextArea;
  */
 public class SummaryTab extends JPanel
 {
-	
+	JTextArea textArea = new JTextArea(40, 88);
+
 	private static final long serialVersionUID = -6708932581784591812L;
 	
 	public SummaryTab()
 	{
-		super(); //Initializing
-		this.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+		super(new GridBagLayout()); //Initializing
+		GridBagConstraints gbc = new GridBagConstraints();
+		//this.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
 		//Add a new Text area to summary tab
-		JTextArea text = new JTextArea(40, 88);
-		text.setText("");
-		text.setEditable(false);
+		textArea.setText("");
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+
+
 		//Creating a scroll pane.
-		JScrollPane scrollableTextArea = new JScrollPane(text, 22, 31);
-		
+		JScrollPane scrollableTextArea = new JScrollPane(textArea, 22, 31);
+
+		//Arrange layout
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.insets = new Insets(5,5,5,5);
+		gbc.fill = GridBagConstraints.BOTH;
+
 		//Add to the Summary Tab
-		add(scrollableTextArea);
+		add(scrollableTextArea, gbc);
+
+
 	}
 
 }
