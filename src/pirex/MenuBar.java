bb4e71092790ef20a48ab981ee2159b86216b761
SHendryx <samuelhendryx@csus.edu>
2018-12-02 23:26:51 -0800

    Added 'EXIT' and 'ABOUT' functionality. Some clean-up needed.

--- src/pirex/MenuBar.java
+++ src/pirex/MenuBar.java
@@ -1,8 +1,15 @@
 package pirex;
-import javax.swing.JButton;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static javax.swing.SwingConstants.CENTER;
 
 /*
  * This class creates JMenu Bar that contains menu buttons.
@@ -29,7 +36,14 @@ public class MenuBar extends JMenuBar {
 		loadQuery = new JMenuItem("Load Query");
 		saveQuery = new JMenuItem("Save Query");
 		open = new JMenuItem("Open");
-		
+
+		//Add Actions to File Menu buttons
+		open.addActionListener(new menuAL());
+		saveQuery.addActionListener(new menuAL());
+		loadQuery.addActionListener(new menuAL());
+		export.addActionListener(new menuAL());
+		exit.addActionListener(new menuAL());
+
 		fileMenu.add(open);
 		fileMenu.add(export);
 		fileMenu.add(loadQuery);
@@ -39,15 +53,22 @@ public class MenuBar extends JMenuBar {
 		//Add Buttons to Options Menu
 		sources = new JMenuItem("Sources");
 		documents = new JMenuItem("Documents");
-		
+
+		//Add Actions to Options Menu buttons
+		sources.addActionListener(new menuAL());
+		documents.addActionListener(new menuAL());
+
 		optionsMenu.add(documents);
 		optionsMenu.add(sources);
-		
-		
+
 		//Add Buttons to Help Menu
 		about = new JMenuItem("About");
 		index = new JMenuItem("Index");
-		
+
+		//Add Actions to Help Menu buttons
+		about.addActionListener(new menuAL());
+		index.addActionListener(new menuAL());
+
 		helpMenu.add(index);
 		helpMenu.add(about);
 		
@@ -55,5 +76,76 @@ public class MenuBar extends JMenuBar {
 		add(fileMenu);
 		add(optionsMenu);
 		add(helpMenu);
+	}
+
+	//Menu Action Listener
+	public class menuAL implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getActionCommand() == "About") {
+				//Open another window that is just a basic "About" window
+				//Main JFrame Setup
+				JFrame aboutFrame = new JFrame("About Pirex");
+				aboutFrame.setLayout(new GridLayout());
+				GridBagConstraints gbc = new GridBagConstraints();
+				aboutFrame.setSize(300,250);
+				aboutFrame.setLocationRelativeTo(null);
+				aboutFrame.setResizable(false);
+				//Setup Icon
+				String iconPath = "src" + File.separator + "assets"  + File.separator + "logo3.png";
+				ImageIcon icon = new ImageIcon(iconPath);
+				aboutFrame.setIconImage(icon.getImage());
+
+				//Main Panel
+				JPanel aboutPanel = new JPanel();
+
+				//Add aboutPanel to aboutFrame
+				aboutFrame.add(aboutPanel);
+
+				//Panel that holds Pirex Image
+				JPanel topPanel = new JPanel();
+
+				//Add Image to topPanel
+				BufferedImage imageFile = null;
+				try {
+					imageFile = ImageIO.read(new File("src/assets/logo3_256x126.png"));
+				} catch (IOException ex) {
+					System.err.println("Error opening image file.");
+					System.exit(1);
+				}
+				//Actually put the image on the topPanel
+				JLabel aboutImage = new JLabel(new ImageIcon(imageFile));
+				aboutImage.setHorizontalAlignment(CENTER);
+				topPanel.add(aboutImage);
+
+				//Panel that holds close button
+				JPanel bottomPanel = new JPanel();
+
+				//Add Panels to aboutPanel
+				aboutPanel.add(topPanel);
+				aboutPanel.add(bottomPanel);
+/*
+				//TextArea that contains About Text
+				JTextArea aboutText = new JTextArea(2,20);
+				aboutText.setText("Pirex: a student company");
+				aboutText.setEditable(false);
+
+				//Add TextArea to bottomPanel
+				bottomPanel.add(aboutText);
+*/
+				//Close button
+				JButton closeAbout = new JButton("Close");
+				closeAbout.addActionListener(this);
+
+				//Add Close button to bottomPanel
+				bottomPanel.add(closeAbout);
+
+				aboutFrame.setVisible(true);
+			}
+
+			if (e.getActionCommand() == "Exit") {
+				System.exit(0);
+			}
+		}
 	}
 }
