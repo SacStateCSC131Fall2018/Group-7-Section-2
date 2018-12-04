c9693dd79047086b0cb9fdd7a897f734f567e503
jsjoh <jsjoh@10.117.252.95>
2018-12-04 13:44:48 -0800

    Resized window to an appropriate size

--- src/pirex/PirexFrame.java
+++ src/pirex/PirexFrame.java
@@ -27,7 +27,7 @@ public class PirexFrame extends JFrame {
 		
 		super("Pirex");
 		setLayout(new BorderLayout());
-		setSize(1080, 720);
+		setSize(640, 480);
 		setResizable(true);
 		String iconPath = "src" + File.separator + "assets"  + File.separator + "logo3.png";
 		ImageIcon icon = new ImageIcon(iconPath);
