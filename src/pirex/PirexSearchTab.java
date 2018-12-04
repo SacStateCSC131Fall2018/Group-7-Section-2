58280a747311d80ca1cdc5b2fb11adfd25b4af19
jsjoh <jsjoh@10.117.252.95>
2018-12-04 14:57:34 -0800

    Replaced "Test" with "    "

--- src/pirex/PirexSearchTab.java
+++ src/pirex/PirexSearchTab.java
@@ -90,7 +90,7 @@ public class PirexSearchTab extends JPanel
 	{
 		JPanel middlePanel2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		GridBagConstraints gbc = new GridBagConstraints();
-		JLabel message = new JLabel("Test");
+		JLabel message = new JLabel("    ");
 		
 		//message.setVisible(false);
 		middlePanel2.add(message);
