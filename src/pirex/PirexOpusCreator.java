d22f04a6800a8cde06a3b83c76bd9ed58c3d5bcf
alanlin <alanlin@10.117.186.138>
2018-12-05 23:13:55 -0800

    By Paragraph - Load Tab Opus Creator

--- src/pirex/PirexOpusCreator.java
+++ src/pirex/PirexOpusCreator.java
@@ -8,31 +8,60 @@ import java.util.List;
 
 public class PirexOpusCreator 
 {
-
 	public static Opus extractOpus(String filename) throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
 		List<String> lines = new ArrayList<String>(); //will store lines
 		String line; //will be used for reading individual lines
-		String title, author;
+		StringBuilder sb = new StringBuilder();
 		
 		
 		while((line = reader.readLine()) != null) 
 		{
-			lines.add(line);
-			// Rest of while loop extracts title and author... inefficiently.
-			if(line.contains("Title: ")) 
+			if(line != "/n")
 			{
-				title = line.substring(7, line.length());
+				if(line.length() > (sb.capacity() - sb.length()))
+				{
+					sb.setLength(line.length() + sb.length());
+				}
+				sb.append(line);
 			}
-			if(line.contains("Author: ")) {
-				author = line.substring(8, line.length());
-			}
+			
+			// Add StringBuilder's string to the ArrayList
+			lines.add(sb.toString());
+			
+			// Empty the StringBuilder
+			sb.setLength(16);
+			sb.delete(0, sb.capacity());
 		}
 		reader.close();
 		
-		Opus opus = new Opus(author, title);
+		Opus opus = new Opus(extractAuthor(lines), extractTitle(lines));
 		
 		return opus;
+	}
+	
+	private static String extractAuthor(List<String> paragraphs) 
+	{
+		for(String temp : paragraphs)
+		{
+			if(temp.contains("Author: "))
+			{
+				return temp.substring(8, temp.length());
+			}
+		}
+		return null;
+	}
+	
+	private static String extractTitle(List<String> paragraphs) 
+	{
+		for(String temp : paragraphs)
+		{
+			if(temp.contains("Title: "))
+			{
+				return temp.substring(7, temp.length());
+			}
+		}
+		return null;
 	}
 }
